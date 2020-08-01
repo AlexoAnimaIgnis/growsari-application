34c1a6f6f2638613e39684d9b8d554788dbecfb7
unknown <ballester.alexander@gmail.com>
2020-08-02 00:53:38 +0800

    updated pom files

--- growsari-util/src/main/java/com/growsari/application/util/SecurityConstants.java
+++ growsari-util/src/main/java/com/growsari/application/util/SecurityConstants.java
@@ -2,4 +2,17 @@ package com.growsari.application.util;
 
 public interface SecurityConstants {
     String SPRING_SECURITY_USER_CACHE = "springSecurityUserCache";
+
+    /**
+     * The default role prefix.
+     */
+    String ROLE_NAME_PREFIX = "ROLE_";
+
+
+    /**
+     * The main role.
+     */
+    String GROWSARI = "GROWSARI";
+
+    String AUTHORITY_PATTERNS = "%s_%s";
 }
