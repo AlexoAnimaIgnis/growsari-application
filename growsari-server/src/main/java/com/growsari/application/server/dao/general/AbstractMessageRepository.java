c97f18567d0ffb548bebe005203ef465fc4d7537
unknown <ballester.alexander@gmail.com>
2020-08-01 13:32:08 +0800

    provided ant build

--- growsari-server/src/main/java/com/growsari/application/server/dao/general/AbstractMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.growsari.application.server.dao.general;
-
-import com.growsari.application.common.model.general.Message;
-import com.growsari.application.server.util.jpa.GrowsariJpaRepository;
-
-public interface AbstractMessageRepository extends GrowsariJpaRepository<Message, String> {
-}
--- growsari-server/src/main/java/com/growsari/application/server/dao/general/AbstractTopicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.growsari.application.server.dao.general;
-
-import com.growsari.application.common.model.general.Topic;
-import com.growsari.application.server.util.jpa.GrowsariJpaRepository;
-
-public interface AbstractTopicRepository extends GrowsariJpaRepository<Topic, String> {
-}
