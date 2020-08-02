f39de18a38a979c16de535efcc07a2b7a4886c49
unknown <ballester.alexander@gmail.com>
2020-08-02 18:32:56 +0800

    updated topic service

--- growsari-server/src/main/java/com/growsari/application/server/service/general/TopicServiceImpl.java
+++ growsari-server/src/main/java/com/growsari/application/server/service/general/TopicServiceImpl.java
@@ -36,9 +36,9 @@ public class TopicServiceImpl implements TopicService {
 
     @Override
     public Topic createTopic(Topic topic) {
-        if (!isTopicUnique(topic.getSubject())) {
-            throw new RuntimeException("Topic: " + topic.getSubject() + " already exists.");
-        }
+//        if (!isTopicUnique(topic.getSubject())) {
+//            throw new RuntimeException("Topic: " + topic.getSubject() + " already exists.");
+//        }
         topic.setModificationId(0);
         return topicRepository.saveAndFlush(topic);
     }
